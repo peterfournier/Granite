565085d16413cb41548153edd5e0878d53609bdd
Peter Fournier <peterfournier356@gmail.com>
2019-05-12 20:06:40 -0400

    Added Ganite.EntityFrameworkCore nuspec, remote Mapper dependencancy and added icon

--- src/Granite.EntityFrameworkCore/BaseRepository.cs
+++ src/Granite.EntityFrameworkCore/BaseRepository.cs
@@ -2,9 +2,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 using Granite.Models;
-using System.Collections;
 using System.Reflection;
 using System.Linq.Expressions;
 using System.Collections.Generic;
