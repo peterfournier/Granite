8e1bfd3789be0517c8233b725ab7557ac44bada0
Peter Fournier <peterfournier356@gmail.com>
2019-06-24 06:32:32 -0400

    wip car sample

---
+++ samples/MyCars/Domain/DTOs/CarDTO.cs
@@ -0,0 +1,14 @@
+﻿using GraniteCore;
+using System;
+using System.Drawing;
+
+namespace MyCars.Domain.DTOs
+{
+    public class CarDTO : BaseDto<Guid, Guid>
+    {
+        public int Year { get; set; }
+        public string ColorHex { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
---
+++ samples/MyCars/Domain/Models/CarEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GraniteCore;
+
+namespace MyCars.Domain.Models
+{
+    public class CarEntity : BaseModel<Guid, Guid>
+    {
+        public int Year { get; set; }
+        public string ColorHex { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
