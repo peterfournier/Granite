d13175a7405932a774d5d96090d69f171188b2db
Peter Fournier <peterfournier356@gmail.com>
2019-10-14 06:06:12 -0400

    testing

--- samples/MyCars/Startup.cs
+++ samples/MyCars/Startup.cs
@@ -66,7 +66,7 @@ namespace MyCars
             services.AddScoped<ICustomerService, CustomerService>();
             // Use Mock
             //services.AddSingleton(typeof(IBaseRepository<,,,>), typeof(MockRepository<,,,>));
-
+            // testing commits...
             // end GraniteCore install
         }
 
